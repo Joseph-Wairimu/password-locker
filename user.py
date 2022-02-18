0501edc2a6f1da0ac8a6a607aeee8b6146544af6
Joseph-Wairimu <josephkamore084@gmail.com>
2022-02-18 22:40:14 +0300

    adding user test file

--- user.py
+++ user.py
@@ -1,21 +1,21 @@
 class user:
-    """
-    Class that generates new instances of user.
-    """
+        """
+        Class that generates new instances of user.
+        """
 
-    user_list=[] # Empty cuser list
-       def __init__(self, first_name,second_name,username,password):
-          '''
-        __init__ method that helps us define properties for our objects.
+        user_list=[] # Empty cuser list
+        def __init__(self, first_name,second_name,username,password):
+            '''
+            __init__ method that helps us define properties for our objects.
 
-        Args:
+            Args:
             first_name: New user first name.
-           second_name : New user second name.
+            second_name : New user second name.
             password: New user password.
             username : New user username.
-        '''
-        self.first_name=first_name
-        self.second_name=second_name
-        self.username=username
-        self.password=password
-       
+            '''
+            self.first_name=first_name
+            self.second_name=second_name
+            self.username=username
+            self.password=password
+            
