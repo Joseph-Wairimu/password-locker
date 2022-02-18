991f550d10fc18b0cd03b65b129d8805fa20de18
Joseph-Wairimu <josephkamore084@gmail.com>
2022-02-19 00:01:37 +0300

    use classmethod decorater search foruser using username

--- user_test.py
+++ user_test.py
@@ -33,7 +33,7 @@ class TestUser(unittest.TestCase):
         test_save_user test case to test if the user object is saved into
          the user list
         '''
-        self.new_user.save_user() # saving the new contact
+        self.new_user.save_user() # saving the new user
         self.assertEqual(len(user.user_list),1)
 
     def tearDown(self):
@@ -48,10 +48,20 @@ class TestUser(unittest.TestCase):
         objects to our contact_list
         '''
         self.new_user.save_user()
-        test_user = user("Test","user","chege254","chege2544") # new contact
+        test_user = user("Test","user","chege254","chege2544") # new user
         test_user.save_user()
         self.assertEqual(len(user.user_list),2)
 
+    def test_delete_user(self):
+        '''
+        test_delete_user to test if we can remove a user from our user list
+        '''
+        self.new_user.save_user()
+        test_user = user("Test","user","chege254","chege2544") # new user
+        test_user.save_user()
+
+        self.new_user.delete_user()# Deleting a user object
+        self.assertEqual(len(user.user_list),1)
 
 
 if __name__ == '__main__':
