bb551e400b247f9f43aa8ae1f7dd97979fd0a001
Joseph-Wairimu <josephkamore084@gmail.com>
2022-02-19 00:58:44 +0300

    function to create new user

--- run.py
+++ run.py
@@ -1,1 +1,8 @@
 from user import user
+
+    def create_user(first_name,second_name,username,password):
+        '''
+        Function to create a new contact
+        '''
+        new_user = user(first_name,second_name,username,password)
+        return new_user
