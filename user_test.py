8db698d849403d1d67bebb8ef88016d2eda2f438
Joseph-Wairimu <josephkamore084@gmail.com>
2022-02-18 22:44:17 +0300

    subclass that defines test cases for user class behaviour

--- user_test.py
+++ user_test.py
@@ -1,2 +1,12 @@
 import unittest # Importing the unittest module
 from user import user
+
+
+class TestUser(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the user class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
