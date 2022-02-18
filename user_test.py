d3d4d641cef820365077b67b40a6cd4d87ebba5f
Joseph-Wairimu <josephkamore084@gmail.com>
2022-02-18 22:57:30 +0300

    first test to confirm initialization

--- user_test.py
+++ user_test.py
@@ -10,3 +10,24 @@ class TestUser(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = user("Joseph","kamore","kamore44","kamore44") # create user object
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.first_name,"Joseph")
+        self.assertEqual(self.new_user.second_name,"kamore")
+        self.assertEqual(self.new_user.username,"kamore44")
+        self.assertEqual(self.new_user.password,"kamore44")
+
+
+if __name__ == '__main__':
+    unittest.main()
