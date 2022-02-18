9472f3b805f79280fca54fd5d7486b616d78b233
Joseph-Wairimu <josephkamore084@gmail.com>
2022-02-19 01:00:04 +0300

    function to save new users

--- run.py
+++ run.py
@@ -2,7 +2,13 @@ from user import user
 
     def create_user(first_name,second_name,username,password):
         '''
-        Function to create a new contact
+        Function to create a new user
         '''
         new_user = user(first_name,second_name,username,password)
         return new_user
+
+    def save_users(user):
+        '''
+        Function to save users
+        '''
+        user.save_user()
