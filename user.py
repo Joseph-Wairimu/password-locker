1d9d7e3dd89bd0f2c17299a86599a25e2d92d9f8
Joseph-Wairimu <josephkamore084@gmail.com>
2022-02-18 23:05:27 +0300

    save_user method to append user details to user_list

--- user.py
+++ user.py
@@ -19,3 +19,10 @@ class user:
             self.username=username
             self.password=password
             
+         def save_user(self):
+
+            '''
+            save_user method saves user objects into user_list
+            '''
+
+            user.user_list.append(self)
