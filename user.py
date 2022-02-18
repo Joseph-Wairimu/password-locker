9264dcdf73b85b305a092ab945d967424fd35aa7
Joseph-Wairimu <josephkamore084@gmail.com>
2022-02-18 22:27:12 +0300

    use init method to pass in properties

--- user.py
+++ user.py
@@ -2,3 +2,10 @@ class user:
     """
     Class that generates new instances of user.
     """
+       def __init__(self, first_name,second_name,username,password):
+    
+        self.first_name=first_name
+        self.second_name=second_name
+        self.username=username
+        self.password=password
+       
