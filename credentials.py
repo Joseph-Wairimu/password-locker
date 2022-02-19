03e783ed89d510cbbae57c8428f537d8dc688228
Joseph-Wairimu <josephkamore084@gmail.com>
2022-02-19 11:20:40 +0300

    init method to add new  credential instance

--- credentials.py
+++ credentials.py
@@ -3,3 +3,12 @@ class Credentials:
     """
     Class that creates new instance for credentials
     """
+
+     
+    def __init__(self,account,username,password):
+        """
+        new application and credentials are being generated
+        """
+        self.account=account
+        self.username=username
+        self.password=password
